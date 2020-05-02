162859ce7b0a683d16b72dc28f9ecfe0f55b1778
kJ <kajitam714@gmail.com>
2020-05-02 23:41:52 +0900

    finished blackbox test by Pester

--- PesterTest/PesterTest1.Tests.ps1
+++ PesterTest/PesterTest1.Tests.ps1
@@ -21,22 +21,144 @@ Describe "Validationの動作確認"{
         It "'A'なら-1が返るはず"{
             Validation 'A' | Should Be -1
         }
+        It "01なら-1が返るはず"{
+            Validation 01 | Should Be -1
+        }
+        It "10なら-1が返るはず"{
+            Validation 10 | Should Be -1
+        }
+        It "11なら-1が返るはず"{
+            Validation 11 | Should Be -1
+        }
     }
 }
 
 Describe "Conjunctionの動作確認" {
     Context "正常系の確認"{
-        It "1+1なら1が返るはず" {
+        It "1 1なら1が返るはず" {
             Conjunction 1 1 | Should Be 1
         }
-        It "1+0なら1が返るはず" {
+        It "1 0なら0が返るはず" {
             Conjunction 1 0 | Should Be 0
         }
-        It "0+1なら1が返るはず" {
+        It "0 1なら0が返るはず" {
             Conjunction 0 1 | Should Be 0
         }
-        It "0+0なら1が返るはず" {
+        It "0 0なら0が返るはず" {
             Conjunction 0 0 | Should Be 0
+        }
+    }
+}
+
+Describe "Disjunctionの動作確認"{
+    Context "正常系の確認"{
+        It "1 1なら1が返るはず"{
+            Disjunction 1 1 | Should Be 1
+        }
+        It "1 0なら1が返るはず"{
+            Disjunction 1 0 | Should Be 1
+        }
+        It "0 1なら1が返るはず"{
+            Disjunction 0 1 | Should Be 1
+        }
+        It "0 0なら0が返るはず"{
+            Disjunction 0 0 | Should Be 0
+        }
+    }
+}
+
+Describe "Negationの動作確認"{
+    Context "正常系の確認"{
+        It "1なら0が返るはず"{
+            Negation 1 | Should Be 0
+        }
+        It "0なら1が返るはず"{
+            Negation 0 | Should Be 1
+        }
+    }
+}
+
+Describe "NonConjunctionの動作確認"{
+    Context "正常系の確認"{
+        It "1 1なら0が返るはず"{
+            NonConjunction 1 1 | Should Be 0
+        }
+        It "1 0なら1が返るはず"{
+            NonConjunction 1 0 | Should Be 1
+        }
+        It "0 1なら1が返るはず"{
+            NonConjunction 0 1 | Should Be 1
+        }
+        It "0 0なら1が返るはず"{
+            NonConjunction 0 0 | Should Be 1
+        }
+    }
+}
+
+Describe "NonDisjunctionの動作確認"{
+    Context "正常系の確認"{
+        It "1 1なら0が返るはず"{
+            NonDisjunction 1 1 | Should Be 0
+        }
+        It "1 0なら0が返るはず"{
+            NonDisjunction 1 0 | Should Be 0
+        }
+        It "0 1なら0が返るはず"{
+            NonDisjunction 0 1 | Should Be 0
+        }
+        It "0 0なら1が返るはず"{
+            NonDisjunction 0 0 | Should Be 1
+        }
+    }
+}
+
+Describe "ExclusiveDisjunctionの動作確認"{
+    Context "正常系の確認"{
+        It "1 1なら0が返るはず"{
+            ExclusiveDisjunction 1 1 | Should Be 0
+        }
+        It "1 0なら1が返るはず"{
+            ExclusiveDisjunction 1 0 | Should Be 1
+        }
+        It "0 1なら1が返るはず"{
+            ExclusiveDisjunction 0 1 | Should Be 1
+        }
+        It "0 0なら1が返るはず"{
+            ExclusiveDisjunction 0 0 | Should Be 0
+        }
+    }
+}
+
+Describe "ConjunctionTestの動作確認"{
+    Context "正常系の確認"{
+        It "1 1なら1が返るはず"{
+            ConjunctionTest 1 1 | Should Be 1
+        }
+        It "1 0なら0が返るはず"{
+            ConjunctionTest 1 0 | Should Be 0
+        }
+        It "0 1なら0が返るはず"{
+            ConjunctionTest 0 1 | Should Be 0
+        }
+        It "0 0なら0が返るはず"{
+            ConjunctionTest 0 0 | Should Be 0
+        }
+    }
+}
+
+Describe "DisjunctionTestの動作確認"{
+    Context "正常系の確認"{
+        It "1 1なら1が返るはず"{
+            DisjunctionTest 1 1 | Should Be 1
+        }
+        It "1 0なら1が返るはず"{
+            DisjunctionTest 1 0 | Should Be 1
+        }
+        It "0 1なら1が返るはず"{
+            DisjunctionTest 0 1 | Should Be 1
+        }
+        It "0 0なら0が返るはず"{
+            DisjunctionTest 0 0 | Should Be 0
         }
     }
 }
--- PesterTest/PesterTest1.ps1
+++ PesterTest/PesterTest1.ps1
@@ -1,10 +1,9 @@
-﻿function Conjunction($a, $b){
-
-    $c = $a * $b
-    return $c
-}
-
+﻿
 function Validation($val){
+    if(([String]$val).Length -ne 1){
+        Write-Host -message "Validation Check Error: please assign length 1 data!"
+        return -1        
+    }
     if(($val -eq 0) -Or ($val -eq 1)){
         return 0
     }
@@ -12,4 +11,61 @@ function Validation($val){
         Write-Host -message "Validation Check Error: please assign 0 or 1!"
         return -1
     }
+}
+function Conjunction($a, $b){
+
+    $c = $a * $b
+
+    return $c
+}
+
+function Disjunction($a, $b){
+    if (($a + $b) -ge 1){
+        return 1
+    }else{
+        return 0
+    }
+}
+
+function Negation($a){
+    if ($a -eq 0){
+        return 1
+    }
+    elseif ($a -eq 1){
+        return 0
+    }
+}
+
+function NonConjunction($a, $b){
+    $c = Conjunction $a $b
+
+    $d = Negation($c)
+    return $d
+}
+
+function NonDisjunction($a, $b){
+
+    $c = Disjunction $a $b
+
+    $d = Negation $c
+    return $d
+}
+
+function ExclusiveDisjunction($a, $b){
+    if ($a + $b -eq 1){
+        return 1
+    }
+    else{
+        return 0
+    }
+}
+
+function ConjunctionTest($a, $b){
+    $c = Conjunction $a $b
+    return $c
+}
+
+function DisjunctionTest($a, $b){
+    $c = Disjunction $a $b
+    return $c
 }
